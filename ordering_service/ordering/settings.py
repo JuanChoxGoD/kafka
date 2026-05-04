28d01582b7ebc3adb794053911026c4cef31775b
Juan Diego Rojas Espitia <jdigoer@gmail.com>
2026-05-04 14:47:26 -0500

    Change default PostgreSQL password
    Updated the default PostgreSQL password in settings.py.

--- ordering_service/ordering/settings.py
+++ ordering_service/ordering/settings.py
@@ -21,7 +21,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB', 'commercial_db'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'Postgres123!'),
         'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
