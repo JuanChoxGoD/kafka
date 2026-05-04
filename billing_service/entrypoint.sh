cb840355a91fd014a73a7b8f9f1772a63dd028d5
Juan Diego Rojas Espitia <jdigoer@gmail.com>
2026-05-03 22:59:06 -0500

    Update entrypoint.sh

--- billing_service/entrypoint.sh
+++ billing_service/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python manage.py run_billing_consumer &
+python manage.py run_billing_consumer >> /proc/1/fd/1 2>&1 &
 exec gunicorn billing.wsgi:application \
     --bind 0.0.0.0:8080 \
     --workers 2 \
