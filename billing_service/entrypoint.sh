4ca12dac53e7a69d23c23dba7134be575cea9d1f
Santiago Torres <santiagotopo@unisabana.edu.co>
2026-05-04 20:01:09 -0500

    migracion db_comercial

--- billing_service/entrypoint.sh
+++ billing_service/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python manage.py migrate --noinput
+
 python manage.py run_billing_consumer > /proc/1/fd/1 2>&1 &
 CONSUMER_PID=$!
 echo "Billing consumer started with PID $CONSUMER_PID"
