e15fb8f0a435f0b9ba77ee1135601ea0e551767e
Santiago Torres <santiagotopo@unisabana.edu.co>
2026-05-04 20:27:33 -0500

    Otras  migraciones

--- shipping_service/entrypoint.sh
+++ shipping_service/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-python manage.py run_shipping_consumer &
+
+python manage.py migrate --noinput
+
+python manage.py run_shipping_consumer > /proc/1/fd/1 2>&1 &
+CONSUMER_PID=$!
+echo "Shipping consumer started with PID $CONSUMER_PID"
+
 exec gunicorn shipping.wsgi:application \
     --bind 0.0.0.0:8080 \
     --workers 2 \
